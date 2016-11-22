223bed738b23ebde838bff4ed28de63aa5772100
thomaskdesign <thomas.kaiaua@gmail.com>
2016-11-21 19:20:00 -0800

    Views Setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name padFinderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the padFinderApp
+ */
+angular.module('padFinderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
